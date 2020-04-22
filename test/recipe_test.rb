24b3a58c9389b92e8effd5b50fb90649cc79bbff
Max Mitrani <mjmitrani168@gmail.com>
2020-04-22 09:36:38 -0600

    Add recipe exists and attributes int method

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -23,7 +23,7 @@ class PantryTest < Minitest::Test
     ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     recipe1 = Recipe.new("Mac and Cheese")
-    assert_equal "Mac and Cheese", pantry.name
+    assert_equal "Mac and Cheese", recipe1.name
   end
 
 
