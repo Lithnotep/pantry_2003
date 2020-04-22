433e487e59add6717f961c999eb67804e1884b45
Max Mitrani <mjmitrani168@gmail.com>
2020-04-22 10:10:03 -0600

    Add cookbook ingredients method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -10,4 +10,12 @@ class CookBook
   def add_recipe(recipe)
     @recipes.push(recipe)
   end
+
+  def ingredients
+    ingredient_names = []
+    @recipes.each do |recipe|
+      recipe.ingredients.map {|ingredient, amount| ingredient_names << ingredient.name}
+    end
+    ingredient_names.uniq
+  end
 end
