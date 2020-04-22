3a261a274da0a3bf412a5ec2bf5870810968f2f2
Max Mitrani <mjmitrani168@gmail.com>
2020-04-22 09:47:56 -0600

    Add recipe add_ingredient method and total_calories test

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,10 +1,15 @@
 require 'pry'
 
 class Recipe
-  attr_reader :name
+  attr_reader :name, :ingredients
 
   def initialize(name)
     @name = name
+    @ingredients = Hash.new(0)
+  end
+
+  def add_ingredient(ingredient, amount)
+    @ingredients[ingredient] += amount
   end
 
 end
