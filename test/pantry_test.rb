4867f86c9cfa7c4d16d32271e2aaa6fdc39842d9
Max Mitrani <mjmitrani168@gmail.com>
2020-04-22 09:21:24 -0600

    Add pantry restock tests

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -27,6 +27,15 @@ class PantryTest < Minitest::Test
     assert_equal 0, pantry.stock_check(ingredient1)
   end
 
+  def test_can_restock
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni",unit: "oz",calories: 200})
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+    assert_equal 15, pantry.stock_check(ingredient1)
+  end
+
 end
 
 
